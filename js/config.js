c81789d6dd42a556c213c4a951c4b372daf9f8e3
Yein Lee <duddnjsglzz@gmail.com>
2025-10-20 13:27:45 +0900

    set cognito user pool2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_XGyT538zK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3siquers848udjnl3a1445cg8e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1ktu4v5nk8ki781v6dlamsvteo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
