0a9a108df4702390df31b5a71f512b0347b420c6
Yein Lee <duddnjsglzz@gmail.com>
2025-10-20 16:13:22 +0900

    reset

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_XGyT538zK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ktu4v5nk8ki781v6dlamsvteo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6tsuidv2r4raejpep1er59pfum', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
